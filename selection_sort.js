228f5f92d61093c74213486a7b49e1ddfcc80a01
narendra chaudhary <56558153+narendra97-bug@users.noreply.github.com>
2024-09-14 16:01:35 +0530

    Add files via upload

--- selection_sort.js
+++ selection_sort.js
@@ -1,19 +1,20 @@
 
 function Selection_sort(arr){
-     let index;
-
-     for(let i=0;i<arr.length-1;i++){
-       let temp=arr[i]
-        for(let j=i+1;j<arr.length;j++){
-           if(temp>arr[j]){
-            temp=arr[j]
-             index=j
-           }
+    for (let i = 0; i < arr.length - 1; i++) {
+        let index = i;  // Initialize the index to the current position
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[index]) {  // Compare and find the minimum
+                index = j;  // Update the index of the minimum value
+            }
         }
-        arr[index]=arr[i]
-        arr[i]=temp
+        // Swap the elements at i and index
+        if (index !== i) {
+            let temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
     }
-    return arr
+    return arr;
 }
 let a1=[1,4,2,6,32,-2,-6,0,16,-70,3]
 console.log(Selection_sort(a1))
